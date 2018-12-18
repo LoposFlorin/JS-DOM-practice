b4bfe914ce8281c2fec7e2a3f9e45e44f08210c1
Lopos Florin <loposflorin@gmail.com>
2018-12-18 21:31:53 +0200

    add style, code correction

--- js/app.js
+++ js/app.js
@@ -1,23 +1,20 @@
 const newBtn = document.getElementById("new-item");
-const input = document.getElementById("inputField");
-
 
 const additem = () => {
-    const itm = document.createElement("li");
-    const itmlist = document.getElementById("list");
-    itmlist.appendChild(itm);
     const getText = document.getElementById("inputFiled").value;
     if(getText <= 0) {
        alert('Please add an item');
     } else {
+       const itmlist = document.getElementById("list");
+       const itm = document.createElement("li");
+       itmlist.appendChild(itm);
        itm.innerHTML = getText;
+       itm.addEventListener("click", clearitem);
     }
-    itm.addEventListener("click", checkitem);
   };
 
 newBtn.addEventListener("click", additem);
 
-
-function checkitem() {
-  this.classList.add("done");
+function clearitem() {
+this.classList.add("done");
 };
